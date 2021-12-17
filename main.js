f59f8b58ff9ca399c599f2b9c1645bf6c2f4a015
Arjun KayalMoni <arjun.umathanu@gmail.com>
2021-12-17 23:17:12 +0530

    Learnt and added a v-bind directive

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks'
-        }
-    }
-})
+	data() {
+		return {
+			product: "Socks",
+			image: "./assets/images/socks_green.jpg",
+			url: "https://www.adidas.co.in/running-ultralight-crew-performance-socks/GI7668.html",
+		};
+	},
+});
