a5f9e6f54167400fa06f0ed8b171fb159c5b046c
Arjun KayalMoni <arjun.umathanu@gmail.com>
2021-12-17 21:18:53 +0530

    Learnt how to create an app instance, mount it and display the options object on the html page

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-const product = 'Socks'
+// const product = 'Socks'
+
+// const app = Vue.createApp({
+// 	data: function () {
+// 		return {
+// 			product: "Socks",
+// 		};
+// 	},
+// });
+
+// const app = Vue.createApp({
+// 	// options object should be at least an empty object
+// 	data: function () {
+// 		return {
+// 			product: "Shoes",
+// 		};
+// 	},
+// });
+
+const app = Vue.createApp({
+	data() {
+		return { product: "Socks", description: "Blue colored woolen socks" };
+	},
+});
